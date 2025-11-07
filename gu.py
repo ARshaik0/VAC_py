45f19512e6ccfcb75497935a33ce7ca266a58606
ARshaik0 <arshaik75@gmail.com>
2025-11-07 19:49:42 +0530

    gg

--- gu.py
+++ gu.py
@@ -1,4 +1,11 @@
 print('hi')
 print(type(1))
+'''git remote add origin https://github.com/ARshaik0/VAC_py.git
+git branch -M main
+git push -u origin main
+git init 
+git add .
+git commit -m 'comments' 
+ '''
 
 
